3c01a06fcd17d2d079f5e8f2c79b1fe2af96250c
Unceasingleek <twalshusmc@gmail.com>
2014-12-04 19:52:54 -0600

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -14,26 +14,6 @@ $(function()
 		};
 	// Accordion script ends here.
 	
-	//Image carousel script starts here.
-	$('#play').click(function() { $('#slider').cycle('resume'); return false; });
-		
-		$('#pause').click(function() { $('#slider').cycle('pause'); return false; });
-
-	$('#slider').cycle({
-		
-		fx:			'scrollHorz',
-		next:		'#next',
-		prev:		'#prev',
-		pager:		'#pager',
-		timeout:	5000,
-		speed:		900
-		
-	});
-	//Image carousel script ends here.
-	//test men starts here
-
-
-	//test men  ends
 });
 
 
